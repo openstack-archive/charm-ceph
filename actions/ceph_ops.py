277b8b0e13bf65e20b13d43b51894ddb3580440b
James Hebden <james.hebden@canonical.com>
2017-08-29 19:07:00 +1000

    Add four new actions to the Ceph charm
    * list-host-osds - lists mounted OSDs for a given unit
    * get-health - outputs ceph -s output
    * disk-for-osd - returns the block device backing a given OSD
    * osd-for-disk - returns the OSD ID (if any) for a given disk
    
    Including unit tests for the above four actions.
    
    Change-Id: Ic9f27d611c0be4f3056d1360d03f93d4d458ac14
    Closes-Bug: #1720099

--- actions/ceph_ops.py
+++ actions/ceph_ops.py
@@ -13,18 +13,21 @@
 # limitations under the License.
 
 from subprocess import CalledProcessError, check_output
+import rados
+import psutil
 import sys
+import os
+import glob
 
 sys.path.append('hooks')
 
-import rados
 from charmhelpers.core.hookenv import log, action_get, action_fail
 from charmhelpers.contrib.storage.linux.ceph import pool_set, \
     set_pool_quota, snapshot_pool, remove_pool_snapshot
 
 
-# Connect to Ceph via Librados and return a connection
 def connect():
+    """Creates a connection to Ceph using librados."""
     try:
         cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
         cluster.connect()
@@ -38,11 +41,13 @@ def connect():
 
 
 def create_crush_rule():
+    """Stub function."""
     # Shell out
     pass
 
 
 def list_pools():
+    """Return a list of all Ceph pools."""
     try:
         cluster = connect()
         pool_list = cluster.list_pools()
@@ -56,7 +61,113 @@ def list_pools():
         action_fail(e.message)
 
 
+def osd_for_disk():
+    """
+    Returns the OSD ID for a given block device.
+
+    Block device is specified by the 'device' action parameter.
+    If the device can not be found, or the 'whoami' file on
+    the OSD's backing device's file system cannot be found,
+    the action will fail, and 'unknown' will be returned.
+    """
+    target_device = action_get("device")
+    disks = psutil.disk_partitions()
+    for dev, mountpoint in disks.iteritems():
+        if dev == target_device:
+            break  # found the device mountpoint
+    if mountpoint:
+        whoami_file = '{}/whoami'.format(mountpoint)
+        with open(whoami_file, 'r') as whoami:
+            try:
+                value = whoami.read().rstrip()
+                return value
+            except IOError:
+                action_fail('Could not read file {}'.format(whoami_file))
+                return "unknown"
+    else:
+        action_fail('Could not find device {}'.format(target_device))
+        return "unknown"
+
+
+def disk_for_osd():
+    """
+    Returns the block device backing an OSD.
+
+    The OSD ID is specified by the 'osd' action parameter.
+    If the block device cannot be determined by using the 'stat'
+    call on the OSD's directory under '/var/lib/ceph/osd' to return
+    the mounted filesystem's backing device, 'unknown' will be returned,
+    and the action will fail.
+    """
+    osd = action_get("osd")
+    whoami_path = '/var/lib/ceph/osd/ceph-{}'.format(osd)
+    try:
+        device = os.stat(whoami_path).st_dev
+    except:
+        action_fail(
+            'Could resolve OSD {}, checked {}'.format(
+                osd, whoami_path
+            ))
+        return 'unknown'
+    else:
+        return device
+
+
+def get_health():
+    """
+    Returns the output of 'ceph health'.
+
+    On error, 'unknown' is returned.
+    """
+    try:
+        value = check_output(['ceph', 'health'])
+        return value
+    except CalledProcessError as e:
+        action_fail(e.message)
+        return 'unknown'
+
+
+def list_host_osds():
+    """
+    Returns a list of OSD IDs currently present on this host.
+
+    Scans /var/lib/ceph/osd to determine this information, and is
+    useful for comparison with the Ceph monitor's OSD list to
+    identify problems with missing or redundant OSD volumes.
+    """
+    osd_ids = []
+    osds = glob.glob('/var/lib/ceph/osd/ceph-[0-9]+')
+    for osd in osds:
+        osd_id = osd.split('-')[-1]
+        try:
+            int(osd_id)
+            osd_ids.extend(osd_id)
+        except:
+            action_fail('Failed to parse OSD ID {}'.format(osd_id))
+            return 'unknown'
+    return osd_ids
+
+
+def list_osds():
+    """Returns a list of OSDs obtained by running 'ceph osd tree'."""
+    try:
+        value = check_output(['ceph', 'osd', 'tree'])
+        return value
+    except CalledProcessError as e:
+        action_fail(e.message)
+
+
 def pool_get():
+    """
+    Returns a key from a pool using 'ceph osd pool get'.
+
+    The key is provided via the 'key' action parameter and the
+    pool provided by the 'pool_name' parameter. These are used when
+    running 'ceph osd pool get <pool_name> <key>', the result of
+    which is returned.
+
+    On failure, 'unknown' will be returned.
+    """
     key = action_get("key")
     pool_name = action_get("pool_name")
     try:
@@ -64,9 +175,18 @@ def pool_get():
         return value
     except CalledProcessError as e:
         action_fail(e.message)
+        return 'unknown'
 
 
 def set_pool():
+    """
+    Sets an arbitrary key key in a Ceph pool.
+
+    Sets the key specified by the action parameter 'key' to the value
+    specified in the action parameter 'value' for the pool specified
+    by the action parameter 'pool_name' using the charmhelpers
+    'pool_set' function.
+    """
     key = action_get("key")
     value = action_get("value")
     pool_name = action_get("pool_name")
@@ -74,6 +194,11 @@ def set_pool():
 
 
 def pool_stats():
+    """
+    Returns statistics for a pool.
+
+    The pool name is provided by the action parameter 'pool-name'.
+    """
     try:
         pool_name = action_get("pool-name")
         cluster = connect()
@@ -92,6 +217,13 @@ def pool_stats():
 
 
 def delete_pool_snapshot():
+    """
+    Delete a pool snapshot.
+
+    Deletes a snapshot from the pool provided by the action
+    parameter 'pool-name', with the snapshot name provided by
+    action parameter 'snapshot-name'
+    """
     pool_name = action_get("pool-name")
     snapshot_name = action_get("snapshot-name")
     remove_pool_snapshot(service='ceph',
@@ -101,6 +233,13 @@ def delete_pool_snapshot():
 
 # Note only one or the other can be set
 def set_pool_max_bytes():
+    """
+    Sets the max bytes quota for a pool.
+
+    Sets the pool quota maximum bytes for the pool specified by
+    the action parameter 'pool-name' to the value specified by
+    the action parameter 'max'
+    """
     pool_name = action_get("pool-name")
     max_bytes = action_get("max")
     set_pool_quota(service='ceph',
@@ -109,6 +248,13 @@ def set_pool_max_bytes():
 
 
 def snapshot_ceph_pool():
+    """
+    Snapshots a Ceph pool.
+
+    Snapshots the pool provided in action parameter 'pool-name' and
+    uses the parameter provided in the action parameter 'snapshot-name'
+    as the name for the snapshot.
+    """
     pool_name = action_get("pool-name")
     snapshot_name = action_get("snapshot-name")
     snapshot_pool(service='ceph',
---
+++ unit_tests/test_actions_osd.py
@@ -0,0 +1,102 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mock import mock, mock_open
+import sys
+
+from test_utils import CharmTestCase
+
+# python-apt is not installed as part of test-requirements but is imported by
+# some charmhelpers modules so create a fake import.
+mock_apt = mock.MagicMock()
+sys.modules['apt'] = mock_apt
+mock_apt.apt_pkg = mock.MagicMock()
+
+# mocking for rados
+mock_rados = mock.MagicMock()
+sys.modules['rados'] = mock_rados
+mock_rados.connect = mock.MagicMock()
+
+# mocking for psutil
+mock_psutil = mock.MagicMock()
+sys.modules['psutil'] = mock_psutil
+mock_psutil.disks = mock.MagicMock()
+
+with mock.patch('charmhelpers.contrib.hardening.harden.harden') as mock_dec:
+    mock_dec.side_effect = (lambda *dargs, **dkwargs: lambda f:
+                            lambda *args, **kwargs: f(*args, **kwargs))
+    # import health actions as actions
+    import ceph_ops as actions
+
+
+class OpsTestCase(CharmTestCase):
+
+    def setUp(self):
+        super(OpsTestCase, self).setUp(
+            actions, ["check_output",
+                      "action_get",
+                      "action_fail",
+                      "open"])
+
+    def test_get_health(self):
+        actions.get_health()
+        cmd = ['ceph', 'health']
+        self.check_output.assert_called_once_with(cmd)
+
+    def test_get_osd_for_disk(self):
+
+        # mock action_get and the disk_partion list
+        # to redirect calls to act on non-existent
+        # mount points
+        self.action_get.return_value = '/dev/null'
+        mock_psutil.disk_partitions.return_value = dict({
+            '/dev/null': '/mnt'
+        })
+
+        # test first for failure. because we've mocked
+        # disk_partitions, we're acting on a nonexistent
+        # mountpoint/device combo, so the action should file
+        # as this path will certainly not be a Ceph OSD
+        actions.osd_for_disk()
+        self.action_fail.assert_called_once
+
+        # now, mock open so that we can force the action to
+        # succeed, and measure that the read call was made
+        # in response to the open succeeding as a measure of
+        # success
+        mocked_open = mock_open(read_data='3\n')
+        mock.patch('self.open', mocked_open, create=True)
+        actions.osd_for_disk()
+        self.assertEqual(self.action_get.call_count, 2)
+        self.assertEqual(self.open.call_count, 2)
+        self.open.assert_called_with('/mnt/whoami', 'r')
+        self.open.read.assert_called_once
+
+    def test_get_disk_for_osd(self):
+        with mock.patch('os.stat') as mocked_stat:
+            self.action_get.return_value = '1'
+            actions.disk_for_osd()
+            mocked_stat.assert_called_once_with('/var/lib/ceph/osd/ceph-1')
+
+    def test_list_host_osds(self):
+        with mock.patch('glob.glob') as mocked_glob:
+            mocked_glob.return_value = [
+                '/var/lib/ceph/osd/ceph-0',
+                '/var/lib/ceph/osd/ceph-1',
+                '/var/lib/ceph/osd/ceph-2',
+            ]
+            actions.list_host_osds()
+            mocked_glob.assert_called_once_with(
+                '/var/lib/ceph/osd/ceph-[0-9]+'
+            )
+            self.assertEqual(len(mocked_glob.return_value), 3)
